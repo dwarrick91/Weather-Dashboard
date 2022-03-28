e470a5334b9367ee563684341372636d9b1567f5
David Warrick <dwarrick91@gmail.com>
2022-03-27 23:06:09 -0400

    added dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,28 +31,41 @@ var currentTempEl = document.querySelector("#currentTemp");
 var currentWindEl = document.querySelector("#currentWind");
 var currentHumidityEl = document.querySelector("#currentHumidity");
 var currentUvIndexEl = document.querySelector("#currentUvIndex");
+
 var today = moment().format("MMM Do, YYYY");
-var dayEl = document.querySelectorAll(".day");
+var day1El = document.querySelector("#day1");
+var day2El = document.querySelector("#day2")
+var day3El = document.querySelector("#day3")
+var day4El = document.querySelector("#day4")
+var day5El = document.querySelector("#day5")
+
+day1El.textContent = moment().add(1, "d").format("MMM Do, YYYY")
+day2El.textContent = moment().add(2, "d").format("MMM Do, YYYY")
+day3El.textContent = moment().add(3, "d").format("MMM Do, YYYY")
+day4El.textContent = moment().add(4, "d").format("MMM Do, YYYY")
+day5El.textContent = moment().add(5, "d").format("MMM Do, YYYY")
+
+
 var dayTempEl = document.querySelectorAll(".dayTemp");
 var dayTempElArray = Array.from(dayTempEl);
 var iconEl = document.querySelectorAll(".icon");
 var iconElArray = Array.from(iconEl);
 var windEl = document.querySelectorAll(".wind");
 var windElArray = Array.from(windEl);
-var uvIndexEl = document.querySelectorAll(".uvIndex");
-var uvIndexElArray = Array.from(uvIndexEl);
+var humidityEl = document.querySelectorAll(".humidity");
+var humidityElArray = Array.from(humidityEl);
 console.log(iconEl);
 function getApi(event) {
   event.preventDefault();
   var city = document.querySelector("#city").value;
   console.log(city);
-  
+
   var weatherUrl =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&appid=" +
     apiKey;
-   
+
   fetch(weatherUrl)
     .then(function (response) {
       return response.json();
@@ -67,26 +80,26 @@ function getApi(event) {
         })
         .then(function (data) {
           console.log(data);
-          
+
           // dataArray = Object.keys(data.daily[i].weather[i])
           // console.log(dataArray);
-          
+
           currentTempEl.textContent = `temperture ${data.current.temp} \u00B0 F`;
           currentWindEl.textContent = `Wind Speed ${data.current.wind_speed} MPH`;
           currentHumidityEl.textContent = `Humidity ${data.current.humidity} %`;
           currentUvIndexEl.textContent = ` UV Index ${data.current.uvi}`;
-          if (currentUvIndexEl >= 8) {
-              currentUvIndexEl.setAttribute("style", "background-color: red")
-          } else if (currentUvIndexEl > 5 && currentUvIndexEl < 8) {
-              currentUvIndexEl.setAttribute("style", "background-color: orange")
-          } else if( currentUvIndexEl > 2 && currentUvIndexEl < 6) {
-              currentUvIndexEl.setAttribute("style", "background-color: yellow")
-          }else{
-              currentUvIndexEl.setAttribute("style", "background-color: green")
+          if (data.current.uvi >= 8) {
+            currentUvIndexEl.setAttribute("style", "background-color: red");
+          } else if (data.current.uvi > 5 && data.current.uvi < 8) {
+            currentUvIndexEl.setAttribute("style", "background-color: orange");
+          } else if (data.current.uvi > 2 && data.current.uvi < 6) {
+            currentUvIndexEl.setAttribute("style", "background-color: yellow");
+          } else {
+            currentUvIndexEl.setAttribute("style", "background-color: green");
           }
 
-          // day1El.textContent = moment().add(1, "d").format("MMM Do, YYYY")
-          for (var i = 0; i < data.daily.length - 3; i++) {
+          
+          for (var i = 0; i < data.daily.length - 2; i++) {
             var day = data.daily[i];
             console.log(day);
             console.log(dayTempElArray);
@@ -96,20 +109,43 @@ function getApi(event) {
             console.log(data.daily[i]);
             console.log(day.weather[0].icon);
 
-            iconElArray[i].src = `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
+            iconElArray[
+              i
+            ].src = `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
             console.log(iconElArray[i]);
             windElArray[i].textContent = `Wind Speed: ${day.wind_speed} MPH`;
             console.log(windElArray);
-            uvIndexElArray[i].textContent = `UV Index: ${day.uvi}`;
-            
+            humidityElArray[i].textContent = `Humidity: ${day.humidity} %`;
+            renderLastSearched();
+            function renderLastSearched() {
+              lastSearchedCity = localStorage.getItem("City");
+
+              city.textContent = lastSearchedCity
+            }
+            searchForm.addEventListener("submit", getApi);
+            event.preventDefault
+
+            localStorage.setItem("City", city);
+            console.log(city);
+            renderLastSearched();
           }
         });
-        
+
       var searchedCity = document.createElement("button");
       searchedCity.textContent = data.name;
+      searchedCity.setAttribute("style", "width: 50%")
+      searchedCity.classList.add("btn")
       searchList.appendChild(searchedCity);
       searchedCity.addEventListener("click", getApi);
-      
     });
 }
 searchForm.addEventListener("submit", getApi);
+
+// saveButton10.addEventListener("click", function (event) {
+//   event.preventDefault();
+
+//   var text10 = document.querySelector("#ten").value;
+
+//   localStorage.setItem("list10", text10);
+//   renderLastSearched();
+// });
